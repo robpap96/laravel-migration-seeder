e6c702f6e26c30f90d33bdc1cba24dc83fc2249b
robpap96 <robertopapagni1996@outlook.it>
2023-01-17 21:43:03 +0100

    fix

--- laravel-migration/database/migrations/2023_01_17_201235_create_trains_table.php
+++ laravel-migration/database/migrations/2023_01_17_201235_create_trains_table.php
@@ -14,12 +14,12 @@ return new class extends Migration
     public function up()
     {
         Schema::create('trains', function (Blueprint $table) {
-            $table->bigIncrements('id');
+            $table->id();
             $table->string('agency', 100);
             $table->string('departure_station', 100);
             $table->string('arrival_station', 100);
-            $table->string('departure_time', 100);
-            $table->string('arrival_time', 100);
+            $table->time('departure_time');
+            $table->time('arrival_time');
             $table->string('train_code', 30);
             $table->integer('carriage_number');
             $table->boolean('in_time');
